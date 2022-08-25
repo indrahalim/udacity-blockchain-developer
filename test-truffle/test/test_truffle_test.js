3bafd9617e4528e778027853b7d08ab922619f79
indrahalim <indra.halimm@gmail.com>
2022-08-26 00:53:30 +0700

    initialize truffle project

---
+++ test-truffle/test/test_truffle_test.js
@@ -0,0 +1,13 @@
+const TestTruffleTest = artifacts.require("TestTruffleTest");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("TestTruffleTest", function (/* accounts */) {
+  it("should assert true", async function () {
+    await TestTruffleTest.deployed();
+    return assert.isTrue(true);
+  });
+});
